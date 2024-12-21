a2c2717b6ab16795454d7839ebe1c52921616fd5
Dev <adeelmazhar409@gmail.com>
2024-12-21 15:28:43 +0500

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,22 +20,25 @@
 
   <!-- Main Content -->
   <div
-    class="relative w-full z-10 gap-5 flex flex-col items-center text-white py-6"
+    class="relative w-full z-10 gap-2 flex flex-col items-center text-white py-6"
   >
     <!-- Logo -->
     <div class="flex justify-center w-full">
       <img src="../logo.png" alt="Logo" class="w-28" />
     </div>
 
+    <div class="mt-6 w-full h-12 bg-black/50">
+
+    </div>
     <!-- Main Text -->
     <div
-      class="text-5xl text-white font-poiret bg-[#2A0B0B] opacity-70 w-full text-center p-6 my-2"
+      class="text-5xl text-white font-poiret bg-[#2A0B0B] opacity-70 w-full text-center p-6 mb-2"
     >
       <p class="text-4xl">Find Your Taste</p>
       <p class="text-lg font-thin">
-        This is a AI website that can aquire your taste by just some
-        option and this can <br />also help you find people with  your similar
-        taste and this website also do steaming of  movies that <br />you choose
+        This is a AI website that can aquire your taste by just some option and
+        this can <br />also help you find people with your similar taste and
+        this website also do steaming of movies that <br />you choose
       </p>
     </div>
 
@@ -55,7 +58,7 @@
       >
         <!-- Option 1: Positioned to the left of the button -->
         <button
-        on:click={()=> goto('mcq')}
+          on:click={() => goto("mcq")}
           class={`absolute -left-[120px] top-1/2 transform -translate-y-1/2 bg-black/40 hover:bg-black text-white px-4 py-2 rounded-full shadow-md transition-transform duration-300 ${
             showOptions ? "translate-x-0" : "-translate-x-10"
           }`}
@@ -65,7 +68,7 @@
 
         <!-- Option 2: Positioned to the right of the button -->
         <button
-        on:click={()=> goto('voice')}
+          on:click={() => goto("voice")}
           class={`absolute -right-28 top-1/2 transform -translate-y-1/2 bg-black/40 hover:bg-black text-white px-4 py-2 rounded-full shadow-md transition-transform duration-300 ${
             showOptions ? "translate-x-0" : "translate-x-10"
           }`}
